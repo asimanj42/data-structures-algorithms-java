1f47cc7fe2d6822823a8842b33723cb84745f0bb
Asiman42 <asimanmemmedli123321@gmail.com>
2023-07-16 21:37:08 +0400

    stack added

---
+++ src/main/java/stack/Stack.java
@@ -0,0 +1,59 @@
+package stack;
+
+public class Stack {
+    private Node top;
+    private int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getTop() {
+        System.out.println("Top: " + top.value);
+    }
+
+    public void getHeight() {
+        System.out.println("Height: " + height);
+    }
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (height != 0) {
+            newNode.next = top;
+        }
+        top = newNode;
+        height++;
+    }
+
+
+    public Node pop() {
+        if (height == 0) {
+            return null;
+        }
+        Node temp = top;// temp = 11
+        top = top.next;
+        temp.next = null;
+        height--;
+        System.out.println("Silinen eded: " + temp.value);
+        return temp; // silinende: top = 3
+    }
+}
