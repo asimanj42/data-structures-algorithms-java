f571d4128f24339e0e69839c3304bace9fbcb56b
Asiman42 <asimanmemmedli123321@gmail.com>
2023-07-17 22:15:30 +0400

    Binary Search Tree added

---
+++ src/main/java/tree/BinarySearchTree.java
@@ -0,0 +1,61 @@
+package tree;
+
+import com.sun.source.tree.BinaryTree;
+
+public class BinarySearchTree {
+    private Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        private Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (value == temp.value) {
+                return false; //binary search tree-de datalar deprecated olmamalidir
+            } else if (value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+
+    }
+
+
+    public boolean contains(int value) {
+        Node temp = root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true; //temp==value
+            }
+        }
+        return false;
+    }
+
+
+}
