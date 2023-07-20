4039599f465c0aee2649407290c5909b5bbd3050
Asiman42 <asimanmemmedli123321@gmail.com>
2023-07-20 20:03:36 +0400

    hashtable added

---
+++ src/main/java/hashtable/Hashtable.java
@@ -0,0 +1,57 @@
+package hashtable;
+
+public class Hashtable {
+    private int size = 7;
+    private Node[] dataMap;
+
+    public Hashtable() {
+        dataMap = new Node[size];
+    }
+
+    public class Node {
+        private String key;
+        private int value;
+        private Node next;
+
+        public Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public void printTable() {
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("   {" + temp.key + "= " + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+
+    public void set(String key, int value) {
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+        if (dataMap[index] == null) {
+            dataMap[index] = newNode;
+        } else {
+            Node temp = dataMap[index];
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+}
